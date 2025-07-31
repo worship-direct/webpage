57861420946c0265e99c633136e43672b4f3a780
gleatd01 <taylor@buzzedtop.com>
2025-07-31 12:42:16 -0400

    Update __init__.py

--- python/kjv/__init__.py
+++ python/kjv/__init__.py
@@ -1,14 +1,10 @@
-# worship_direct/kjv/__init__.py
 import os
 import json
-from .verse_accessor import VerseAccessor
+from worship_direct.verse_accessor import VerseAccessor
 
 json_path = os.path.join(os.path.dirname(__file__), 'kjv.json')
 with open(json_path, 'r', encoding='utf-8') as f:
     bible_data = json.load(f)
 
-# Expose books as attributes
 globals().update({book: VerseAccessor(chapters) for book, chapters in bible_data.items()})
-
-# For generic usage
 bible = VerseAccessor(bible_data)
