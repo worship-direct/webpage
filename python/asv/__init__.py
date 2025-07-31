6d5bbf3a9514d3e2008f8067d6fb11fa698f3dd8
gleatd01 <taylor@buzzedtop.com>
2025-07-31 12:50:52 -0400

    Update __init__.py

--- python/asv/__init__.py
+++ python/asv/__init__.py
@@ -2,9 +2,12 @@ import os
 import json
 from worship_direct.verse_accessor import VerseAccessor
 
-json_path = os.path.join(os.path.dirname(__file__), 'asv.json')
-with open(json_path, 'r', encoding='utf-8') as f:
-    bible_data = json.load(f)
-
-globals().update({book: VerseAccessor(chapters) for book, chapters in bible_data.items()})
-bible = VerseAccessor(bible_data)
+json_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'bible', 'asv.json')
+try:
+    with open(json_path, 'r', encoding='utf-8') as f:
+        bible_data = json.load(f)
+    globals().update({book: VerseAccessor(chapters) for book, chapters in bible_data.items()})
+    bible = VerseAccessor(bible_data)
+except FileNotFoundError:
+    bible_data = {}
+    bible = VerseAccessor(bible_data)
