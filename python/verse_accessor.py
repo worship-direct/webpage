4938050090fcbc57474c839f417905c20aa6e8bc
gleatd01 <taylor@buzzedtop.com>
2025-07-31 12:41:20 -0400

    Update verse_accessor.py

--- python/verse_accessor.py
+++ python/verse_accessor.py
@@ -1,11 +1,8 @@
-# ksv/verse_accessor.py
-
 class VerseAccessor:
     def __init__(self, data):
         self._data = data
 
     def __getattr__(self, name):
-        # Try title-case for book names, else direct numeric string for chapters/verses
         key = name.title() if name.title() in self._data else name
         if key in self._data:
             val = self._data[key]
@@ -15,7 +12,6 @@ class VerseAccessor:
         raise AttributeError(f"No such book/chapter/verse: {name}")
 
     def __getitem__(self, key):
-        # Also support item access: b['John']['3']['16']
         key = str(key)
         if key in self._data:
             val = self._data[key]
