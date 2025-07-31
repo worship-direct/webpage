2efc4820cb9aa4fba942960e864121806ff0edf2
gleatd01 <taylor@buzzedtop.com>
2025-07-31 12:39:27 -0400

    Update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,14 @@
+# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name='kjv',
-    version='0.1.0',
+    name='worship-direct',
+    version='0.2.0',
     packages=find_packages(),
-    package_data={'kjv': ['kjv.json']},
+    package_data={
+        'worship_direct.kjv': ['kjv.json'],
+        'worship_direct.asv': ['asv.json'],
+    },
     include_package_data=True,
-    description='KJV Bible access via attribute lookup',
-    author='Your Name',
-    install_requires=[],
-    python_requires='>=3.6',
-)p
+    # ... other metadata
+)
