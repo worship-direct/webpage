dadd6518495ec1d6babe2bd23da8b75596e88ecd
gleatd01 <taylor@buzzedtop.com>
2025-07-31 12:42:54 -0400

    Update __init__.py

--- python/asv/__init__.py
+++ python/asv/__init__.py
@@ -1,7 +1,6 @@
-# worship_direct/asv/__init__.py
 import os
 import json
-from .verse_accessor import VerseAccessor
+from worship_direct.verse_accessor import VerseAccessor
 
 json_path = os.path.join(os.path.dirname(__file__), 'asv.json')
 with open(json_path, 'r', encoding='utf-8') as f:
